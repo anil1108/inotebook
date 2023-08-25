546384eef99cdbe2ef7cb34d15a5dd2c88526331
Anil <110411755+anil1108@users.noreply.github.com>
2023-08-25 10:27:32 +0530

    created validation for users

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
 const connectToMongo = () => {
-    mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.5")
+    mongoose.connect("mongodb://127.0.0.1:27017/inotebook?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.5")
     .then(()=>{
         console.log("databse connected successfully")
     })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
-
+const { body, validationResult } = require('express-validator');
 // creating a user using : POST "/api/auth/" . Doesn't require Auth.
 
 
 // setting the output 1) It can be a json object or any
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/', [
+    body('name','Enter a valid name').isLength({ min: 3 }),
+    body('email','Enter a valid email').isEmail(),
+    body('password','it should be, min 3').isLength({ min: 5 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+    })
+    .then(user => res.json(user))
+    .catch(err => {console.log(err)
+    res.json({error:'Please enter unique value for email', message: err.message})})
 })
 
 module.exports = router;
